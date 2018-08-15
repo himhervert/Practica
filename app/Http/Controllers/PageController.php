4f79391d79ef6ddb3e9fb2e20dd5c776fdf07bca
himhervert <leohervert@gmail.com>
2018-08-15 13:50:17 -0500

    Se agrego PageController y se modifico web.php

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -1,3 +1,21 @@
 <?php
 
-namespace App\Http\Controller;
+namespace App\Http\Controllers;
+
+class PageController extends Controller
+{
+    public function about()
+    {
+        return "About Us Page";
+    }
+
+    public function anuncios()
+    {
+        return "Anuncios Page";
+    }
+    public function contact()
+    {
+        return "Sumited Page";
+    }
+    
+}
--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,8 @@
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+Route::get('/anuncios','PageController@anuncios')->name('anuncios');
+
+Route::get('/about','PageController@about')->name('about');
+Route::post('/contact','PageController@submitContact');
