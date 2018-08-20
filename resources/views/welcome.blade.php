1f37d4550f6e15e4b462ae106d34f92eafe2a189
Leonardo Iván Hervert Morales <halo_269@hotmail.com>
2018-08-20 03:03:11 -0500

    Se agrego el template principal.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,8 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link href="{{ asset('css/app.css') }} " rel="stylesheet" type="text/css">
+        <link href="{{ asset('css/additional.css') }} " rel="stylesheet" type="text/css">
         <title>Laravel</title>
 
         <!-- Fonts -->
